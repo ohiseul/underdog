410ac55d3829b2dc73308edb1a7e708da8569618
“ohiseul” <dltmf3140@gmail.com>
2023-07-13 10:31:18 +0900

    js실습

---
+++ js/index.js
@@ -0,0 +1,72 @@
+alert("ok");
+console.log(50);
+console.log(50-3);
+
+let a = 50;
+let b = 3;
+a=b;
+a= 50000;
+
+console.log(a,b);
+console.log("안녕하세요");
+
+
+moviesㅣist = [];
+
+
+a = "10";
+b = "안녕하세요";
+let c = true;
+
+c = false;
+
+console.log(a,b,c);
+console.log(10,"10");
+
+let happyFace = "🥰";
+
+console.log(happyFace);
+
+
+console.log(5+3);
+console.log(5-3);
+console.log(5/3);
+console.log(5*3);
+console.log(30%4);
+console.log(3 ** 2);
+console.log("3" * 5);
+
+
+console.log(5>3);
+console.log(5<3);
+console.log(5==3);
+console.log(3 != 4);
+console.log(3 >= 4);
+
+let date = new Date();
+console.log(date);
+
+let hour = date.getHours();
+console.log(hour);
+
+if(hour <12 ){
+    console.log("오전입니다.")
+}else if(hour<15){
+    console.log("오후입니다.")
+}else{
+    console.log("저녁입니다")
+}
+
+a=50;
+switch(a%2){
+    case 0:
+        console.log("짝수다");
+        break;
+    case 1:
+        console.log("홀수다");
+        break;
+    default:
+        break;
+
+
+}
