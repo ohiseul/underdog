a637c9a067893fd19cc5ccaa32c1383455f53448
“ohiseul” <dltmf3140@gmail.com>
2023-07-13 13:36:37 +0900

    js실습3

--- js/index.js
+++ js/index.js
@@ -1,149 +1,173 @@
 alert("ok");
-console.log(50);
-console.log(50-3);
+// console.log(50);
+// console.log(50-3);
 
-let a = 50;
-let b = 3;
-a=b;
-a= 50000;
+// let a = 50;
+// let b = 3;
+// a=b;
+// a= 50000;
 
-console.log(a,b);
-console.log("안녕하세요");
-
-
-moviesㅣist = [];
+// console.log(a,b);
+// console.log("안녕하세요");
 
 
-a = "10";
-b = "안녕하세요";
-let c = true;
-
-c = false;
-
-console.log(a,b,c);
-console.log(10,"10");
-
-let happyFace = "🥰";
-
-console.log(happyFace);
+// moviesㅣist = [];
 
 
-console.log(5+3);
-console.log(5-3);
-console.log(5/3);
-console.log(5*3);
-console.log(30%4);
-console.log(3 ** 2);
-console.log("3" * 5);
+// a = "10";
+// b = "안녕하세요";
+// let c = true;
+
+// c = false;
+
+// console.log(a,b,c);
+// console.log(10,"10");
+
+// let happyFace = "🥰";
+
+// console.log(happyFace);
 
 
-console.log(5>3);
-console.log(5<3);
-console.log(5==3);
-console.log(3 != 4);
-console.log(3 >= 4);
-
-let date = new Date();
-console.log(date);
-
-let hour = date.getHours();
-console.log(hour);
-
-if(hour <12 ){
-    console.log("오전입니다.")
-}else if(hour<15){
-    console.log("오후입니다.")
-}else{
-    console.log("저녁입니다")
-}
-
-a=50;
-switch(a%2){
-    case 0:
-        console.log("짝수다");
-        break;
-    case 1:
-        console.log("홀수다");
-        break;
-    default:
-        break;
+// console.log(5+3);
+// console.log(5-3);
+// console.log(5/3);
+// console.log(5*3);
+// console.log(30%4);
+// console.log(3 ** 2);
+// console.log("3" * 5);
 
 
-}
+// console.log(5>3);
+// console.log(5<3);
+// console.log(5==3);
+// console.log(3 != 4);
+// console.log(3 >= 4);
 
-let input = prompt("숫자를 입략하세요")
-console.log(typeof(input));
-input = Number(input);
-console.log(typeof(input));
+// let date = new Date();
+// console.log(date);
 
-switch(input % 2){
-    case 0:
-        console.log("짝수")
-        break;
-    case 1:
-        console.log("홀수")
-        break;
-    default:
-        console.log("숫자가 아닙니다")
-        break;
+// let hour = date.getHours();
+// console.log(hour);
+
+// if(hour <12 ){
+//     console.log("오전입니다.")
+// }else if(hour<15){
+//     console.log("오후입니다.")
+// }else{
+//     console.log("저녁입니다")
+// }
+
+// a=50;
+// switch(a%2){
+//     case 0:
+//         console.log("짝수다");
+//         break;
+//     case 1:
+//         console.log("홀수다");
+//         break;
+//     default:
+//         break;
+
+
+// }
+
+// let input = prompt("숫자를 입략하세요")
+// console.log(typeof(input));
+// input = Number(input);
+// console.log(typeof(input));
+
+// switch(input % 2){
+//     case 0:
+//         console.log("짝수")
+//         break;
+//     case 1:
+//         console.log("홀수")
+//         break;
+//     default:
+//         console.log("숫자가 아닙니다")
+//         break;
         
 
-}
+// }
 
-let dayOfWeek = [ "monday", "tuesday","wednesday","thursday","friday","saturday","sunday",54, true, false]
-console.log(dayOfWeek[1]);
+// let dayOfWeek = [ "monday", "tuesday","wednesday","thursday","friday","saturday","sunday",54, true, false]
+// console.log(dayOfWeek[1]);
 
-let myInfo = ["직업","거주지",29,true]
+// let myInfo = ["직업","거주지",29,true]
 
-let obj ={
-    name: "이름",
-    age:29,
-    occupation:"직업",
-    isMarried:false,
-    favoriteMovies:["영화1","영화2"],
-    favoriteFoods:[
-        {
-            name: "햄버거",
-            isHealthy:false
-        },
+// let obj ={
+//     name: "이름",
+//     age:29,
+//     occupation:"직업",
+//     isMarried:false,
+//     favoriteMovies:["영화1","영화2"],
+//     favoriteFoods:[
+//         {
+//             name: "햄버거",
+//             isHealthy:false
+//         },
         
-        {
-            name: "피자",
-            isHealthy:false
-        }
+//         {
+//             name: "피자",
+//             isHealthy:false
+//         }
 
-    ]
-}
+//     ]
+// }
 
-console.log(obj.name);
-console.log(obj.occupation);
-console.log(obj.favoriteFoods[1]);
-
-
-function sayHello(name){
-    console.log("Hello", name)
-
-}
-
-sayHello();
-sayHello("kim");
-
-function addNum(a,b){
-    console.log(a+b);
-    return a+b;
-}
-
-let result = addNum(2,3);
-console.log(result);
+// console.log(obj.name);
+// console.log(obj.occupation);
+// console.log(obj.favoriteFoods[1]);
 
 
-let obj2 = {
-    name:"이름",
-    age:29,
-    sayHello: function(){
-        return "Hello";
+// function sayHello(name){
+//     console.log("Hello", name)
+
+// }
+
+// sayHello();
+// sayHello("kim");
+
+// function addNum(a,b){
+//     console.log(a+b);
+//     return a+b;
+// }
+
+// let result = addNum(2,3);
+// console.log(result);
+
+
+// let obj2 = {
+//     name:"이름",
+//     age:29,
+//     sayHello: function() {
+//         console.log(`Hello ${this.name} you are ${this.age} years old`)
+//         return "Hello";
+//     }
+
+// }
+
+// console.log(obj2.sayHello());
+
+for(i=0; i<30;i++){
+    if(i%2 ==0){
+        console.log(i)
+    }else if(i%5==0){
+        console.log('5읩 배수이다')
+    }else{
     }
-
 }
 
-console.log(obj2.sayHello());
+let arr = ['사과','배','복숭아','바나나','딸기'];
+
+for(let i=0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+
+for(let i of arr){
+    console.log(i);
+}
+
+for(let i in arr){
+    console.log(arr[i]);
+}
